11d4735943c8e65caf8269009324b52c272250a6
nat <crawfon@email.sc.edu>
2025-08-30 14:59:52 -0400

    optimize photo loading?

--- nats_website/next.config.ts
+++ nats_website/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
+
+module.exports = {
+  output: 'export',
+  images: { unoptimized: true },
+};
+
 
 export default nextConfig;
